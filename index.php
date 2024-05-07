d413cc0d5756d0a41bd8d2e30fe68c81862e351a
Geert Roks <31696350+GeertRoks@users.noreply.github.com>
2024-05-07 15:43:05 +0200

    Bug fix: index.php still had old customize api variable

--- index.php
+++ index.php
@@ -7,6 +7,6 @@
         <?php wp_head(); ?>
     </head>
     <body class="iframe-wrapper"> 
-        <iframe class="external-site-view" src="<?php print esc_url(get_theme_mod('my_custom_url')); ?>">
+        <iframe class="external-site-view" src="<?php print esc_url(get_theme_mod('frontend_url')); ?>">
     </body>
 </html>
